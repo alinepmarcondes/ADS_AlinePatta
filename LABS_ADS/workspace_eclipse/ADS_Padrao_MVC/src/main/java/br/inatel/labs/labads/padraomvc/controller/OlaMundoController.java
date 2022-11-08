1b05cb878d12e5a8a03dcfab6388e5667b22abc5
Aline <aline.patta@inatel.br>
2022-11-08 09:23:22 -0300

    Aula08_11

---
+++ LABS_ADS/workspace_eclipse/ADS_Padrao_MVC/src/main/java/br/inatel/labs/labads/padraomvc/controller/OlaMundoController.java
@@ -0,0 +1,18 @@
+package br.inatel.labs.labads.padraomvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OlaMundoController {
+	
+	@GetMapping("/ola")
+	public String getOla(Model model) {
+		
+		model.addAttribute("usuario", "Aline");
+		
+		return "ola-mundo";
+	}
+	
+}
